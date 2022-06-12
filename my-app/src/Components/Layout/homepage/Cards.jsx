5f32c9c1c2f1754dbe7240b205a260861fd1a1c5
prachi.sachdevaa <prachi.sachdevaa@gmail.com>
2022-06-12 23:45:24 +0530

    cards Design change

--- my-app/src/Components/Layout/homepage/Cards.jsx
+++ my-app/src/Components/Layout/homepage/Cards.jsx
@@ -11,29 +11,29 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/img 1.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae pulvinar augue, id tempus nulla. Integer dictum accumsan molestie. Suspendisse ut ultrices libero, quis vulputate orci. Nullam feugiat non diam eu sagittis. Phasellus ultricies feugiat luctus. Aliquam in ultricies arcu.'
+              text='The portal is very user friendly in terms of searching resumes and job postings. Also, they have a good database to search resumes. As far as services are concerned its excellent! Their turnaround time for resolving any issue is just a few minutes and that is really appreciable.Thank you so much for all your effort.'
               path='/'
             />
             <CardItem
               src='images/img 2.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae pulvinar augue, id tempus nulla. Integer dictum accumsan molestie. Suspendisse ut ultrices libero, quis vulputate orci. Nullam feugiat non diam eu sagittis. Phasellus ultricies feugiat luctus. Aliquam in ultricies arcu.'
+              text='This is an excellent job portal, we value the resumes received through this channel. It provides relevant and accurate matches as per the job openings. Magic Search and Power search are extremely useful tools.'
               path='/'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img 3.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae pulvinar augue, id tempus nulla. Integer dictum accumsan molestie. Suspendisse ut ultrices libero, quis vulputate orci. Nullam feugiat non diam eu sagittis. Phasellus ultricies feugiat luctus. Aliquam in ultricies arcu.'
+              src='images/img 1.jpg'
+              text='The Websites database has been one of our reliable sources for recruitment, backed by a very efficient team who would go out of their way to make sure that requests are taken ahead with immediate and complete closure.'
               path='/'
             />
             <CardItem
               src='images/img 4.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae pulvinar augue, id tempus nulla. Integer dictum accumsan molestie. Suspendisse ut ultrices libero, quis vulputate orci. Nullam feugiat non diam eu sagittis. Phasellus ultricies feugiat luctus. Aliquam in ultricies arcu.'
+              text='The Websites database has been one of our reliable sources for recruitment, backed by a very efficient team who would go out of their way to make sure that requests are taken ahead with immediate and complete closure.'
               path='/'
             />
             <CardItem
-              src='images/img 3.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae pulvinar augue, id tempus nulla. Integer dictum accumsan molestie. Suspendisse ut ultrices libero, quis vulputate orci. Nullam feugiat non diam eu sagittis. Phasellus ultricies feugiat luctus. Aliquam in ultricies arcu.'
+              src='images/img 2.jpg'
+              text='The Websites database has been one of our reliable sources for recruitment, backed by a very efficient team who would go out of their way to make sure that requests are taken ahead with immediate and complete closure.'
               path='/'
             />
           </ul>
