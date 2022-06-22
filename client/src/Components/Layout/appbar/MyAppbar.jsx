0091110e85f209dd2ba74c0786f43b3fc451c353
Naman Jain <namanjain5499@gmail.com>
2022-06-22 16:48:53 +0530

    Login Icon Fix

--- client/src/Components/Layout/appbar/MyAppbar.jsx
+++ client/src/Components/Layout/appbar/MyAppbar.jsx
@@ -1,7 +1,7 @@
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
-import LoginIcon from '@mui/icons-material/Login';
-import HomeIcon from '@mui/icons-material/Home';
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import LoginIcon from "@mui/icons-material/Login";
+import HomeIcon from "@mui/icons-material/Home";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -20,25 +20,24 @@ import UserMenu from "./UserMenu";
 // import { useCookies } from 'react-cookie';
 // import jwt from 'jsonwebtoken'
 
-
-
 export default function MyAppbar() {
   const { isAuth } = useSelector((state) => state.login);
   // let jobs = useSelector(state=>state.search.searched)
 
-
-
-
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar elevation={0} style={{ background: "white" , overflow:"auto" }} position="fixed">
+        <AppBar
+          elevation={0}
+          style={{ background: "white", overflow: "auto" }}
+          position="fixed"
+        >
           <Toolbar className={styles.toolbar}>
             <Box className={styles.header_left}>
               <Box className={styles.link}>
                 <Typography
                   sx={{ fontWeight: "bold" }}
-                  className={styles.icons }
+                  className={styles.icons}
                 >
                   <Link to="/">
                     <HomeIcon
@@ -49,7 +48,7 @@ export default function MyAppbar() {
                 </Typography>
                 <Typography
                   sx={{ fontWeight: "bold" }}
-                  className={styles.iconsmobile }
+                  className={styles.iconsmobile}
                 >
                   <Link to="/viewjobs">
                     <BusinessCenterIcon
@@ -60,7 +59,7 @@ export default function MyAppbar() {
                 </Typography>
                 <Typography
                   sx={{ fontWeight: "bold" }}
-                  className={styles.iconsmobile }
+                  className={styles.iconsmobile}
                 >
                   <Link to="/companies">
                     <AssignmentTurnedInIcon
@@ -70,7 +69,7 @@ export default function MyAppbar() {
                   </Link>
                 </Typography>
 
-                <Link style={{ textDecoration: "none" }}  to="/viewjobs">
+                <Link style={{ textDecoration: "none" }} to="/viewjobs">
                   <Typography
                     sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
                     className={styles.typography}
@@ -96,16 +95,16 @@ export default function MyAppbar() {
                 <Link to="/login">
                   <Typography
                     sx={{ fontSize: "1.2rem" }}
-                    className={styles.typography}
+                    className={[styles.typography]}
                   >
                     Login
                   </Typography>
-                  <Typography>
-                  <LoginIcon
-                  fontSize="large"
-                  style={{ color: "#127c71" }}
-                ></LoginIcon>
-                </Typography>
+                  <Typography className={styles.iconsmobile}>
+                    <LoginIcon
+                      fontSize="large"
+                      style={{ color: "#127c71" }}
+                    ></LoginIcon>
+                  </Typography>
                 </Link>
               )}
             </Box>
